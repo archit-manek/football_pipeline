33aaa932ce90683cc0049f60c5be7b867a543186
Archit Manek <architmanek@gmail.com>
2025-07-01 22:52:30 -0700

    added enrichments to silver

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 # filepath: utils/config.py
 from pathlib import Path
 
-# Define directories for the bronze layer
+# Define directories (BRONZE)
 BRONZE_DIR = Path("data/bronze")
 BRONZE_DIR_MATCHES = BRONZE_DIR / "matches"
 BRONZE_DIR_COMPETITIONS = BRONZE_DIR / "competitions"
@@ -9,10 +9,18 @@ BRONZE_DIR_LINEUPS = BRONZE_DIR / "lineups"
 BRONZE_DIR_EVENTS = BRONZE_DIR / "events"
 BRONZE_DIR_360_EVENTS = BRONZE_DIR / "360_events"
 
-# Ensure directories exist
+# Define directories (SILVER)
+SILVER_DIR = Path("data/silver")
+SILVER_DIR_EVENTS = SILVER_DIR / "events"
+
+# Ensure directories exist (BRONZE)
 BRONZE_DIR.mkdir(parents=True, exist_ok=True)
 BRONZE_DIR_MATCHES.mkdir(parents=True, exist_ok=True)
 BRONZE_DIR_COMPETITIONS.mkdir(parents=True, exist_ok=True)
 BRONZE_DIR_LINEUPS.mkdir(parents=True, exist_ok=True)
 BRONZE_DIR_EVENTS.mkdir(parents=True, exist_ok=True)
 BRONZE_DIR_360_EVENTS.mkdir(parents=True, exist_ok=True)
+
+# Ensure directories exist (SILVER)
+SILVER_DIR.mkdir(parents=True, exist_ok=True)
+SILVER_DIR_EVENTS.mkdir(parents=True, exist_ok=True)
