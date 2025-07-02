63f4e30848812cfe93f89434d5e600d87cdbc6f7
Archit Manek <architmanek@gmail.com>
2025-07-01 21:41:13 -0700

    added logging and improved silver

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import logging
 import os
 from bronze.bronze_ingest import bronze_ingest
 from silver.silver_transform import enrich_pass_data
@@ -10,5 +9,5 @@ os.makedirs("logs/silver", exist_ok=True)
 
 if __name__ == "__main__":
     bronze_ingest()
-    # enrich_pass_data()
+    enrich_pass_data()
     # gold_transform()
