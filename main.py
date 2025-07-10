6df5116523686904b434c1ef1262c24d8cdaa98f
Archit Manek <architmanek@gmail.com>
2025-07-09 20:29:57 -0700

    added gold layer - xG model

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from _02_silver.competitions_data import process_competitions_data
 from _02_silver.lineups_data import process_lineups_data
 from _02_silver.matches_data import process_matches_data
 from _02_silver._360_events import process_360_events_data
+from _03_gold.xg_model import build_xg_model
 
 # Set up logging
 os.makedirs("logs/bronze", exist_ok=True)
@@ -23,3 +24,7 @@ if __name__ == "__main__":
 
     # GOLD STAGE
     # aggregate_match_data()
+    
+    # Build xG model
+    
+    # build_xg_model()  # Uncomment to build full model
